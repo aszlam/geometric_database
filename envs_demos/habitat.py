4410235866c74a32f238037535a404085405c12e
Mahi Shafiullah <notmahi@fb.com>
2022-06-08 13:46:01 -0700

    Black formatting of code

--- envs_demos/habitat.py
+++ envs_demos/habitat.py
@@ -23,12 +23,12 @@ class SemanticSegmentationDataset(Dataset):
         # funtion that takes the raw output of the semantic sensor and creates
         # a 2d numpy array of out class labels.
         self.labels = {
-            'background': 0,
-            'wall': 1,
-            'floor': 2,
-            'ceiling': 3,
-            'chair': 4,
-            'table': 5,
+            "background": 0,
+            "wall": 1,
+            "floor": 2,
+            "ceiling": 3,
+            "chair": 4,
+            "table": 5,
         }
         self.instance_id_to_name = self.extractor.instance_id_to_name
         self.poses = self.extractor.poses
@@ -43,20 +43,19 @@ class SemanticSegmentationDataset(Dataset):
         sample = self.extractor[idx]
         # self.extractor.poses gives you the pose information
         # (both x y z and also quarternions)
-
-        raw_semantic_output = sample['semantic']
+        raw_semantic_output = sample["semantic"]
         truth_mask = self.get_class_labels(raw_semantic_output)
 
         output = {
-            'rgb': sample['rgba'],
-            'truth': truth_mask.astype(int),
-            'depth': sample['depth'],
+            "rgb": sample["rgba"],
+            "truth": truth_mask.astype(int),
+            "depth": sample["depth"],
         }
 
         if self.transforms:
-            output['rgb'] = self.transforms(output['rgb'])
-            output['truth'] = self.transforms(output['truth']).squeeze(0)
-            output['depth'] = self.transforms(output['depth']).squeeze(0)
+            output["rgb"] = self.transforms(output["rgb"])
+            output["truth"] = self.transforms(output["truth"]).squeeze(0)
+            output["depth"] = self.transforms(output["depth"]).squeeze(0)
 
         return output
 
@@ -66,13 +65,17 @@ class SemanticSegmentationDataset(Dataset):
 
 if __name__ == "__main__":
     # Replace with the path to your scene file
-    SCENE_FILEPATH = '/private/home/notmahi/data/replica_dataset/apartment_0/habitat/mesh_semantic.ply'
+    SCENE_FILEPATH = "/private/home/notmahi/data/replica_dataset/apartment_0/habitat/mesh_semantic.ply"
     BATCH_SIZE = 6
     # Run basic sanity test on the dataloader.
-    extractor = ImageExtractor(SCENE_FILEPATH, output=['rgba', 'semantic', 'depth'], pose_extractor_name="panorama_extractor")
+    extractor = ImageExtractor(
+        SCENE_FILEPATH,
+        output=["rgba", "semantic", "depth"],
+        pose_extractor_name="panorama_extractor",
+    )
 
-    dataset = SemanticSegmentationDataset(extractor,
-        transforms=transforms.Compose([transforms.ToTensor()])
+    dataset = SemanticSegmentationDataset(
+        extractor, transforms=transforms.Compose([transforms.ToTensor()])
     )
 
     # Create a Dataloader to batch and shuffle our data
@@ -84,19 +87,18 @@ if __name__ == "__main__":
             for i, img in enumerate(imgs):
                 ax = plt.subplot(1, batch_size, i + 1)
                 ax.axis("off")
-                if img_type == 'rgb':
+                if img_type == "rgb":
                     plt.imshow(img.numpy().transpose(1, 2, 0))
-                elif img_type == 'truth':
+                elif img_type == "truth":
                     plt.imshow(img.numpy())
                 else:
                     plt.imshow(img.numpy())
 
             plt.savefig(f"{k}_fig.png", dpi=200)
 
-        batch_size = len(sample_batch['rgb'])
+        batch_size = len(sample_batch["rgb"])
         for k in sample_batch.keys():
             show_row(sample_batch[k], batch_size, k)
-
 
     _, sample_batch = next(enumerate(dataloader))
     show_batch(sample_batch)
