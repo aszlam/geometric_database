1e12505e7f6a7244348bd86263c6d6f773d493d4
Mahi Shafiullah <3000253+notmahi@users.noreply.github.com>
2022-09-11 11:37:13 -0700

    Created detailed environment setup

---
+++ install.sh
@@ -0,0 +1,15 @@
+# Run these commands to create your conda environment.
+# conda create -n geodb python=3.8
+# conda activate geodb
+
+conda install -y pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch-lts -c nvidia
+conda install -y habitat-sim withbullet headless -c conda-forge -c aihabitat
+pip install -r requirements.txt
+
+# Install the hashgrid encoder.
+cd gridencoder
+module load cuda/11.1
+export CUDA_HOME=/public/apps/cuda/11.1
+python setup.py install
+cd ..
+
